fc2ed008eaeaef4b549924696f71072040c67561
dangelo.simone <dangelosimone29@gmail.com>
2025-09-22 17:13:36 +0200

    checkbox books

--- routes/borrow_books_route.py
+++ routes/borrow_books_route.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from config.config import get_databases
 
 borrowBlueprint = Blueprint("borrowBooks", __name__)
 
 
 @borrowBlueprint.get("/borrow_books")
 def borrow_books():
-    return render_template('borrow_books.html')
+    databases = get_databases()
+    books_cursor = databases['books'].find({"copy": {"$gt": 0}})
+
+    books = list(books_cursor)
+
+    pagetitle = "Prestito Libri"
+
+    return render_template('borrow_books.html',books=books, pagetitle=pagetitle)
+
+@borrowBlueprint.post("/borrow_books")
+def handler_borrow_books():
+    lista_scelte = request.form.getlist("scelte")
+    print(lista_scelte)
+
