9354421bb11658b313bceacdcba02dd0541635aa
dangelo.simone <dangelosimone29@gmail.com>
2025-09-23 18:34:59 +0200

    borrow books list

--- routes/borrowed_books_route.py
+++ routes/borrowed_books_route.py
@@ -1,8 +1,31 @@
 from flask import Blueprint, render_template
+from db.DatabaseSingleton import get_databases
 
 borrowedBlueprint = Blueprint("borrowedBooks", __name__)
+
+databases = get_databases()
+books_collection = databases["books"]
 
 
 @borrowedBlueprint.get("/borrowed_books")
 def borrowed_books():
-    return render_template('borrowed_books.html')
+
+    pipeline = [
+        {
+            "$lookup": {
+                "from": "students",
+                "localField": "isbn",
+                "foreignField": "isbn",
+                "as": "borrowed_by"
+            }
+        },
+        {
+            "$match": {
+                "borrowed_by.0": {"$exists": True}  # only books to borrow
+            }
+        }
+    ]
+
+    books = list(books_collection.aggregate(pipeline))
+    return render_template("borrowed_books.html", books=books)
+
